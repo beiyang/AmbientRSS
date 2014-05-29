977953d5fb221f87009fca9bb8984e484585375f
Bei Yang <bei.yang@disney.com>
2014-05-28 19:05:54 -0700

    no message

--- js/ambient-rss.js
+++ js/ambient-rss.js
@@ -1,3 +1,28 @@
-var ContentElement = Backbone.View.extend({
+var ambientRSS = {};
 
+ambientRSS.defaultTemplate = _.template('<div class="ambient-content-block"><div class="thumb"><%= image %></div>' +
+    '<div class="ambient-title"><%= title %></div><div class="ambient-description"><%= description %></div></div>');
+
+ambientRSS.ContentElement = Backbone.View.extend({
+    template: ambientRSS.defaultTemplate,
+    render: function(){
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
+
+var g_feed;
+$(document).ready(function(){
+    $.getFeed({
+        url:'http://www.engadget.com/rss.xml',
+        success: function(feed){
+            g_feed=feed;
+            var col = new Backbone.Collection(feed.items);
+            col.each(function(m){
+                var test = $("<div>").appendTo("#tester");
+                var view = new ambientRSS.ContentElement({model:m, el:test});
+                view.render();
+            });
+        }
+    });
 });
